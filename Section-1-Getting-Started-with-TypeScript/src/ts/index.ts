ad857c558d260f5ad19d56e90119198d9f73e188
Peter Čagala <petercagala@gmail.com>
2020-04-23 16:58:07 +0200

    [Section1-7] definiton of interface and defining types

--- Section-1-Getting-Started-with-TypeScript/src/ts/index.ts
+++ Section-1-Getting-Started-with-TypeScript/src/ts/index.ts
@@ -2,7 +2,27 @@ import axios from "axios";
 
 const url = "https://jsonplaceholder.typicode.com/todos/1";
 
+interface Todo {
+    // We can ignore properties
+    // "userId": number,
+    "id": number,
+    "title": string,
+    "completed": boolean
+}
+
 axios.get(url)
     .then(response => {
-        console.log(response.data);
-    })
+        // const myData: Todo = response.data;
+        // We have to options, how the type define
+        const myData = response.data as Todo;
+
+        const id: number = myData.id;
+        const title: string = myData.title;
+        const completed: boolean = myData.completed;
+
+        console.log(`
+            id = ${id},
+            title = ${title},
+            completed = ${completed}
+        `);
+    });
