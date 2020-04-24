d2b429d0cf43b7633e1290f28bb10eea5b4ee4d0
Peter Čagala <petercagala@gmail.com>
2020-04-24 16:46:43 +0200

    [Section5-29] arrays in TypeScript

---
+++ Section-5-Mastering-Typed-Arrays/src/arrays/arrays.ts
@@ -0,0 +1,12 @@
+let carMakes: string[] = ['BMW'];
+
+// ...carMakes: rozlej to ako yablko
+carMakes= [...carMakes, 'Ford', 'Toyota', 'Skoda'];
+
+const dates: Date[] = [new Date(), new Date()];
+
+const carsByMake: string[][] = [
+    ['car1'],
+    ['car2'],
+    ['car3']
+];
