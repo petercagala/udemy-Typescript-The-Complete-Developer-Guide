37531af61f275e42379d39ee67aa75140d4759c1
Peter Čagala <petercagala@gmail.com>
2020-04-24 10:47:08 +0200

    [Section3-16] Annotations with variables

---
+++ Section-3-Types-Annotation-in-Action/src/annotations/variables.ts
@@ -0,0 +1,13 @@
+// : number -> this is a type annotation
+let apples: number = 5;
+
+apples= 45;
+
+let speed: string = "fast speed";
+
+let hasName: boolean = true;
+
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+let now: Date = new Date();
