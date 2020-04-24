88fe7dde6c91a5f45cf1128e83914665412a823f
Peter Čagala <petercagala@gmail.com>
2020-04-24 11:26:22 +0200

    [Section3-16-2] Object literal annotations

--- Section-3-Types-Annotation-in-Action/src/annotations/variables.ts
+++ Section-3-Types-Annotation-in-Action/src/annotations/variables.ts
@@ -11,3 +11,31 @@ let nothingMuch: null = null;
 let nothing: undefined = undefined;
 
 let now: Date = new Date();
+
+// Array
+const colors: string[] = ['red', 'green', 'blue'];
+const numbers: number[] = [1,2,3,4,5];
+const truths: boolean[] = [true, false, true];
+
+// Classes
+class Car {
+
+}
+
+let car1: Car = new Car();
+
+
+
+// Objects Literals: Ak nedefinujes aj pour Literal, tak potom do jeho atributov  mozes zadat co chces
+// a to nechces
+let point1: {x: number; y: number;} = {
+    x: 10,
+    y: 20
+};
+
+let point2 = {
+    x: "fzdhzdfhzd", // sem mozem kludne dat bez definicie co chcem a to je na prd
+    y: 20
+};
+
+
