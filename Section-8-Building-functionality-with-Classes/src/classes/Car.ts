bc9517d7fd23b512764aea0349e6c0bbeea5aa59
Peter Čagala <petercagala@gmail.com>
2020-04-28 06:24:31 +0200

    [section8-49] Fields in inheritance

--- Section-8-Building-functionality-with-Classes/src/classes/Car.ts
+++ Section-8-Building-functionality-with-Classes/src/classes/Car.ts
@@ -2,6 +2,23 @@ import {Vehicle} from "./Vehicle";
 
 export class Car extends Vehicle {
 
+    private _wheels: number;
+
+
+    constructor(color: string, wheels: number) {
+        super(color);
+        this._wheels = wheels;
+    }
+
+
+    get wheels(): number {
+        return this._wheels;
+    }
+
+    set wheels(value: number) {
+        this._wheels = value;
+    }
+
     public drive(): void {
         console.log("Hello Car");
 
@@ -9,7 +26,8 @@ export class Car extends Vehicle {
         // this.color = 'green';
 
         // getter
-        console.log(`Vehicle has colour: ${this.color}`)
+        console.log(`Vehicle has colour: ${this.color}`);
+        console.log(`Vehicle has wheels: ${this.wheels}`);
     }
 
 }
--- Section-8-Building-functionality-with-Classes/src/classes/index.ts
+++ Section-8-Building-functionality-with-Classes/src/classes/index.ts
@@ -1,7 +1,7 @@
 import {Vehicle} from './Vehicle';
 import {Car} from './Car';
 
-const vehicle: Vehicle = new Car('orange');
+const vehicle: Vehicle = new Car('orange', 12);
 vehicle.drive();
 
 // we don's see this method as it's private
