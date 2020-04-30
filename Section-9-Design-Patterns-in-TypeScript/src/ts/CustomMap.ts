36a830bbead0cf03743e1bc31e3780a4f1517642
Peter Čagala <petercagala@gmail.com>
2020-04-30 07:04:07 +0200

    [section9-63] adding markers

--- Section-9-Design-Patterns-in-TypeScript/src/ts/CustomMap.ts
+++ Section-9-Design-Patterns-in-TypeScript/src/ts/CustomMap.ts
@@ -1,3 +1,7 @@
+import {User} from './User';
+import {Company} from "./Company";
+import LatLng = google.maps.LatLng;
+
 export class CustomMap {
     private _googleMap: google.maps.Map;
 
@@ -15,5 +19,20 @@ export class CustomMap {
 
     get googleMap(): google.maps.Map {
         return this._googleMap;
+    }
+
+    addUserMarker(user: User): void {
+        new google.maps.Marker({
+                map: this.googleMap,
+                position: {
+                    lat: user.location.latitude,
+                    lng: user.location.longitude,
+                }
+            }
+        );
+    }
+
+    addCompanyMarker(company: Company): void {
+
     }
 }
--- Section-9-Design-Patterns-in-TypeScript/src/ts/index.ts
+++ Section-9-Design-Patterns-in-TypeScript/src/ts/index.ts
@@ -2,5 +2,8 @@ import {User, redColour} from './User';
 import {Company} from "./Company";
 import {CustomMap} from './CustomMap';
 
+const user: User = new User();
+
 const customMap: CustomMap = new CustomMap("map");
+customMap.addUserMarker(user);
 
