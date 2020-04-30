bc5041d53d42107ae953f80b5e63c3ad997af9bb
Peter Čagala <petercagala@gmail.com>
2020-04-30 09:35:08 +0200

    [section9-64] duplicate code

--- Section-9-Design-Patterns-in-TypeScript/src/ts/CustomMap.ts
+++ Section-9-Design-Patterns-in-TypeScript/src/ts/CustomMap.ts
@@ -33,6 +33,13 @@ export class CustomMap {
     }
 
     addCompanyMarker(company: Company): void {
-
+        new google.maps.Marker({
+                map: this.googleMap,
+                position: {
+                    lat: company.location.latitude,
+                    lng: company.location.longitude,
+                }
+            }
+        );
     }
 }
--- Section-9-Design-Patterns-in-TypeScript/src/ts/index.ts
+++ Section-9-Design-Patterns-in-TypeScript/src/ts/index.ts
@@ -3,7 +3,10 @@ import {Company} from "./Company";
 import {CustomMap} from './CustomMap';
 
 const user: User = new User();
+const company: Company = new Company();
 
 const customMap: CustomMap = new CustomMap("map");
 customMap.addUserMarker(user);
+customMap.addCompanyMarker(company);
+
 
