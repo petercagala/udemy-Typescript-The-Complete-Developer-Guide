fa2d57e0c70b511c629b46d27e955d1bf18fde35
Peter Čagala <petercagala@gmail.com>
2020-04-30 05:53:41 +0200

    [section9-60] using google.maps: exploring type definition types

--- Section-9-Design-Patterns-in-TypeScript/src/ts/index.ts
+++ Section-9-Design-Patterns-in-TypeScript/src/ts/index.ts
@@ -1,21 +1,21 @@
-import {User, redColour} from './User';
-import {Company} from "./Company";
+// import {User, redColour} from './User';
+// import {Company} from "./Company";
 
-// if we import some variable withou {}, it should be default export variable, the name could be whatever
-import greenDefaultVariable from './User';
 
-const user: User = new User();
 
-const company: Company = new Company();
 
-console.log(`Default variable is: ${greenDefaultVariable}`);
-console.log(`variable User is: ${user}`);
-console.log(user);
-console.log(`variable redColour is: ${redColour}`);
-
-console.log(company);
 
 // we have to help typeScript understand, that 'google' is an global variable
 // npm install @types/googlemaps
+// nemusime to importovat
+const mapDiv = document.getElementById('map') as HTMLElement;
 
-google
+new google.maps.Map(mapDiv,
+    {
+        zoom: 10,
+        center: {
+            lat: 0,
+            lng: 0,
+            // new google.maps.LatLng(1, 1);
+        },
+    });
