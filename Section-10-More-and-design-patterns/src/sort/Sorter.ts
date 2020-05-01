6b03eea44a1035750753831f97280451326bcd22
Peter Čagala <petercagala@gmail.com>
2020-05-01 14:47:25 +0200

    [section10-80] Type guards :::     instanceof: for any non-primitive type,  typeof: primitive types

--- Section-10-More-and-design-patterns/src/sort/Sorter.ts
+++ Section-10-More-and-design-patterns/src/sort/Sorter.ts
@@ -20,19 +20,34 @@ export class Sorter {
         // vytiahne length automaticky
         const {length: lengthOfArray} = this.sortCollection;
 
-
-        // if collection is type of array numbers
         for (let i = 0; i < lengthOfArray; i++) {
             for(let j = 0; j < lengthOfArray - i - 1; j++) {
-                if(this.sortCollection[j] > this.sortCollection[j + 1]) {
-                    const leftHand: number = this.sortCollection[j];
-                    this.sortCollection[j] = this.sortCollection[j+1];
-                    this.sortCollection[j+1] = leftHand;
+
+                // if collection is type of array numbers
+                // Type guard: this.sortCollection instanceof Array
+                if(this.sortCollection instanceof Array) {
+                    if(this.sortCollection[j] > this.sortCollection[j + 1]) {
+                        const leftHand: number = this.sortCollection[j];
+                        this.sortCollection[j] = this.sortCollection[j+1];
+                        this.sortCollection[j+1] = leftHand;
+                    }
                 }
+
+                // if collection is type of string
+                // Type guard: typeof this.sortCollection === 'string'
+                if(typeof this.sortCollection === 'string') {
+
+                }
+
+
             }
         }
 
-        //if collection is type of string, we will provide this logic instead
+
+
+
+
+
 
 
     }
