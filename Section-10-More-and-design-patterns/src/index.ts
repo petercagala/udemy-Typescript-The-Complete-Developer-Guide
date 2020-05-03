e3abac5090693a1c063c1decf61802b514c7feda
Peter Čagala <petercagala@gmail.com>
2020-05-03 07:11:55 +0200

    [section10-87] Linked List implementation

--- Section-10-More-and-design-patterns/src/index.ts
+++ Section-10-More-and-design-patterns/src/index.ts
@@ -1,12 +1,25 @@
 import {Sorter} from './sort/Sorter';
 import {NumbersCollection} from "./sort/sortable/NumbersCollection";
 import {CharactersCollection} from "./sort/sortable/CharactersCollection";
+import {LinkedList} from './sort/sortable/LinkedList';
 
 const sorterNumberArray: Sorter = new Sorter(new NumbersCollection([300,150, 10, 3, -5, 0]));
 const sorterString: Sorter = new Sorter(new CharactersCollection('Cagala'));
 
+let linkedList: LinkedList = new LinkedList();
+linkedList.add(10);
+linkedList.add(3);
+linkedList.add(-5);
+linkedList.add(0);
+const sorterLinkedList: Sorter = new Sorter(linkedList);
+
 sorterNumberArray.sort();
 sorterString.sort();
+sorterLinkedList.sort();
 
 console.log(sorterNumberArray.sortCollection);
 console.log(sorterString.sortCollection);
+
+if(sorterLinkedList.sortCollection instanceof LinkedList) {
+    console.log(sorterLinkedList.sortCollection.print());
+}
---
+++ Section-10-More-and-design-patterns/src/sort/sortable/LinkedList.ts
@@ -0,0 +1,95 @@
+import {Sortable} from "./Sortable";
+import {Node} from "./Node";
+
+export class LinkedList implements Sortable{
+
+    private _head: Node | null = null;
+
+
+    get head(): Node | null {
+        return this._head;
+    }
+
+    set head(value: Node | null) {
+        this._head = value;
+    }
+
+    add(data: number): void {
+        if(!this.head) {
+            this.head = new Node(data);
+            return;
+        }
+
+        let tail = this.head;
+        while(tail.next) {
+            tail = tail.next;
+        }
+        tail.next = new Node(data);
+    }
+
+    compare(leftIndex: number, rightIndex: number): boolean {
+        if(!this.head) {
+            throw new Error('List is empty!');
+        }
+
+        return this.at(leftIndex).data > this.at(rightIndex).data;
+    }
+
+    getLength(): number {
+
+        if(!this.head) {
+            return 0;
+        }
+
+        let length: number = 1;
+        let node = this.head;
+
+        while(node.next) {
+            node = node.next;
+            length ++;
+        }
+        return length;
+    }
+
+    at(index: number):  Node {
+
+        if(!this.head) {
+            throw new Error('Index out of bounds!');
+        }
+
+        let internalIndex = 0;
+        let node: Node | null = this.head;
+
+        while(node) {
+            if(index === internalIndex) {
+                return node;
+            }
+            node = node.next;
+            internalIndex ++;
+        }
+        throw new Error('Index out of bounds!');
+    }
+
+    swap(leftIndex: number, rightIndex: number): void {
+        const leftNode: Node = this.at(leftIndex);
+        const rightNode: Node = this.at(rightIndex);
+
+        const leftHand: number = leftNode.data;
+        leftNode.data = rightNode.data;
+        rightNode.data = leftHand;
+    }
+
+    print(): void {
+        if(!this.head) {
+            console.log(this);
+            return;
+        }
+
+        let node: Node | null = this.head;
+        while(node) {
+            console.log(node.data);
+            node = node.next;
+        }
+    }
+
+}
