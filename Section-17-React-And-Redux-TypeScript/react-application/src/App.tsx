3497b22ce517c19fdfefd21198dee1636b3b4544
Peter Čagala <petercagala@gmail.com>
2020-05-05 18:42:52 +0200

    [section17-261] Functional Componets

--- Section-17-React-And-Redux-TypeScript/react-application/src/App.tsx
+++ Section-17-React-And-Redux-TypeScript/react-application/src/App.tsx
@@ -12,51 +12,68 @@ interface AppState {
 }
 
 /**
- * Musis zadefinovat React.Component<> genericke typy pre:
- * Props:
- * State:
+ * Definition as FUNCTIONAL COMPONENT
+ * @constructor
  */
-class App extends React.Component<AppProps, AppState> {
-    /**
-     * We were redefining state property, which was set up at React.Component<AppProps, AppState>
-     * @param props
-     */
-    // state: AppState = {
-    //     counter: 0,
-    // }
-
-    constructor(props: AppProps) {
-        super(props);
-
-        // In this case, we have to define the state type in generic definition React.Component<AppProps, AppState>
-        this.state = {
-            counter: 0,
-        }
-    }
+const App = (props: AppProps): JSX.Element => {
 
 
-    private handleIncrement = (): void => {
-        this.setState({
-            counter: this.state.counter + 1,
-        })
-    };
+    return (
+        <div>
+            {props.color}
+        </div>
+    );
+}
 
-    private handleDecrement = (): void => {
-        this.setState({
-            counter: this.state.counter - 1,
-        })
-    }
 
-    render() {
-        return (
-            <div>
-                <button onClick={this.handleIncrement}>Increment</button>
-                <button onClick={this.handleDecrement}>Decrement</button>
-                <br/>
-                {this.state.counter}
-            </div>
-        );
-    }
-};
+//
+// /**
+//  * Definition as CLASS COMPONENT
+//  * Musis zadefinovat React.Component<> genericke typy pre:
+//  * Props:
+//  * State:
+//  */
+// class App extends React.Component<AppProps, AppState> {
+//     /**
+//      * We were redefining state property, which was set up at React.Component<AppProps, AppState>
+//      * @param props
+//      */
+//     // state: AppState = {
+//     //     counter: 0,
+//     // }
+//
+//     constructor(props: AppProps) {
+//         super(props);
+//
+//         // In this case, we have to define the state type in generic definition React.Component<AppProps, AppState>
+//         this.state = {
+//             counter: 0,
+//         }
+//     }
+//
+//
+//     private handleIncrement = (): void => {
+//         this.setState({
+//             counter: this.state.counter + 1,
+//         })
+//     };
+//
+//     private handleDecrement = (): void => {
+//         this.setState({
+//             counter: this.state.counter - 1,
+//         })
+//     }
+//
+//     render() {
+//         return (
+//             <div>
+//                 <button onClick={this.handleIncrement}>Increment</button>
+//                 <button onClick={this.handleDecrement}>Decrement</button>
+//                 <br/>
+//                 {this.state.counter}
+//             </div>
+//         );
+//     }
+// };
 
 export default App;
