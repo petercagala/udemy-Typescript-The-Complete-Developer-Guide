e6b81d33a97c79ca865d142be142b4586c4ee1e5
Peter Čagala <petercagala@gmail.com>
2020-05-05 18:43:52 +0200

    [section17-262-1] Back to Class Components

--- Section-17-React-And-Redux-TypeScript/react-application/src/App.tsx
+++ Section-17-React-And-Redux-TypeScript/react-application/src/App.tsx
@@ -15,15 +15,15 @@ interface AppState {
  * Definition as FUNCTIONAL COMPONENT
  * @constructor
  */
-const App = (props: AppProps): JSX.Element => {
-
-
-    return (
-        <div>
-            {props.color}
-        </div>
-    );
-}
+// const App = (props: AppProps): JSX.Element => {
+//
+//
+//     return (
+//         <div>
+//             {props.color}
+//         </div>
+//     );
+// }
 
 
 //
@@ -33,47 +33,47 @@ const App = (props: AppProps): JSX.Element => {
 //  * Props:
 //  * State:
 //  */
-// class App extends React.Component<AppProps, AppState> {
-//     /**
-//      * We were redefining state property, which was set up at React.Component<AppProps, AppState>
-//      * @param props
-//      */
-//     // state: AppState = {
-//     //     counter: 0,
-//     // }
-//
-//     constructor(props: AppProps) {
-//         super(props);
-//
-//         // In this case, we have to define the state type in generic definition React.Component<AppProps, AppState>
-//         this.state = {
-//             counter: 0,
-//         }
-//     }
-//
-//
-//     private handleIncrement = (): void => {
-//         this.setState({
-//             counter: this.state.counter + 1,
-//         })
-//     };
-//
-//     private handleDecrement = (): void => {
-//         this.setState({
-//             counter: this.state.counter - 1,
-//         })
-//     }
-//
-//     render() {
-//         return (
-//             <div>
-//                 <button onClick={this.handleIncrement}>Increment</button>
-//                 <button onClick={this.handleDecrement}>Decrement</button>
-//                 <br/>
-//                 {this.state.counter}
-//             </div>
-//         );
-//     }
-// };
+class App extends React.Component<AppProps, AppState> {
+    /**
+     * We were redefining state property, which was set up at React.Component<AppProps, AppState>
+     * @param props
+     */
+    // state: AppState = {
+    //     counter: 0,
+    // }
+
+    constructor(props: AppProps) {
+        super(props);
+
+        // In this case, we have to define the state type in generic definition React.Component<AppProps, AppState>
+        this.state = {
+            counter: 0,
+        }
+    }
+
+
+    private handleIncrement = (): void => {
+        this.setState({
+            counter: this.state.counter + 1,
+        })
+    };
+
+    private handleDecrement = (): void => {
+        this.setState({
+            counter: this.state.counter - 1,
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.handleIncrement}>Increment</button>
+                <button onClick={this.handleDecrement}>Decrement</button>
+                <br/>
+                {this.state.counter}
+            </div>
+        );
+    }
+};
 
 export default App;
