481a7fc559fba4159f938c60810e48f24bb5ef92
Peter Čagala <petercagala@gmail.com>
2020-05-06 06:18:26 +0200

    [section17-262-5] npm install @types/react-redux

--- Section-17-React-And-Redux-TypeScript/react-application/src/index.tsx
+++ Section-17-React-And-Redux-TypeScript/react-application/src/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {createStore, applyMiddleware, combineReducers} from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+
+const store = createStore(reducers, applyMiddleware(thunk));
+
 
 ReactDOM.render(
     // <React.StrictMode>
     //     <App/>
     // </React.StrictMode>,
-    <App color="red"/>,
+    <Provider store = {store}>
+        <App color="red"/>
+    </Provider>,
     // document.getElementById('root')
     document.querySelector("#root")
 );
