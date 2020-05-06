4e207266cae200e70f0d09800af4bb4d3e1556e0
Peter Čagala <petercagala@gmail.com>
2020-05-06 10:47:34 +0200

    [section17-266] A reducer with Enum

---
+++ Section-17-React-And-Redux-TypeScript/react-application/src/reducers/todos.ts
@@ -0,0 +1,13 @@
+import {Todo, FetchTodosAction} from '../actions/index';
+import {ActionTypes} from '../actions/types'
+
+export const todosReducer = (state: Todo[] = [],
+                             action: FetchTodosAction
+) => {
+    switch (action.type) {
+        case ActionTypes.fetchTodos:
+            return action.type;
+        default:
+            return state;
+    }
+};
